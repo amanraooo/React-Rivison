5d7af82c669ca1dc7b26de2d2fc6dc3e6ef818dc
amanraooo <ak9070777@gmail.com>
2025-09-19 23:19:23 +0530

    navbar project

--- week1/router-project-starter/src/pages/Login.jsx
+++ week1/router-project-starter/src/pages/Login.jsx
@@ -6,8 +6,8 @@ const Login = ({ setIsLoggedIn }) => {
   return (
     <Template
       title="Welcome Back"
-      desc1="Build skills for today, tomorrow, and beyond"
-      desc2="Education to future-proof your career"
+      desc1="Build skills for today, tomorrow, and beyond."
+      desc2="Education to future-proof your career."
       image={loginImg}
       setIsLoggedIn={setIsLoggedIn}
       formtype="login"
