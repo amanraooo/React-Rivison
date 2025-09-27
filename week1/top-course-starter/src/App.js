cdd7602127054f0dce957fe5d6c3b5b127b04b4d
amanraooo <ak9070777@gmail.com>
2025-09-27 14:54:10 +0530

    top courses project

--- week1/top-course-starter/src/App.js
+++ week1/top-course-starter/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Navbar from './components/Navbar';
+import Filter from './components/Filter';
+import Cards from './components/Cards';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      {/* navbar */}
+      <Navbar/>
+
+      {/* all filters */}
+      <Filter/>
+
+      {/* all cards */}
+      <Cards/>
+
+    </div>);
 };
 
 export default App;
