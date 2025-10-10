d79560ba4db0cbb878a68e732e5dc35383282f13
amanraooo <ak9070777@gmail.com>
2025-10-11 00:08:41 +0530

    top-courses project

--- week1/top-course-starter/src/components/Filter.jsx
+++ week1/top-course-starter/src/components/Filter.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
-const Filter = ({filterData, category, setCategory}) => {
-  function filterHandler(title){
+const Filter = ({ filterData, category, setCategory }) => {
+  function filterHandler(title) {
     setCategory(title);
   }
   return (
     <div className="w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center">
       {filterData.map((data) => {
-        return <button
-        onClick={()=>filterHandler(data.title)}
-         key={data.id}
-         className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 hover:text-black border-2 transition-all duration-300`}
-         >{data.title}</button>;
+        return (
+          <button
+            onClick={() => filterHandler(data.title)}
+            key={data.id}
+            className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 hover:text-white border-2 transition-all duration-300
+${
+  category === data.title
+    ? "bg-ipacity-60 border-white"
+    : "bg-ipacity-60 border-transparent"
+}`}
+          >
+            {data.title}
+          </button>
+        );
       })}
     </div>
   );
