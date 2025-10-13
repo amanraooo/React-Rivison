ab133cd8b4878b2815655e30fe516f0b7bcccab3
amanraooo <ak9070777@gmail.com>
2025-10-13 17:10:59 +0530

    testimonial slider project

--- week1/Testimonial-slider/src/components/Card.jsx
+++ week1/Testimonial-slider/src/components/Card.jsx
@@ -1,39 +1,37 @@
 import React from "react";
-import {FaQuoteLeft,FaQuoteRight, FaGem} from "react-icons/fa";
-
+import { FaQuoteLeft, FaQuoteRight, FaGem } from "react-icons/fa";
 
 const Card = (props) => {
   let reviews = props.reviews;
   return (
     <div className="flex flex-col md:relative">
-
       <div className="absolute top-[-7rem] z-[10] mx-auto">
-        <img className="aspect-square rounded-full w-[140px] h-[140px] z-[25]"
-        src={reviews.image} />
-        <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-[25] hover:shadow-xl"
+          src={reviews.image}
+        />
+        <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px] hover:shadow-lg"></div>
       </div>
 
       <div className="text-center mt-7">
-        <p className="font-bold text-2xl ">{reviews.name} </p>
-      </div>
+        <p className="font-bold text-2xl capitalize tracking-wider">
+          {reviews.name}{" "}
+        </p>
 
-      <div className="text-center mt-1">
         <p className="text-violet-300 uppercase text-sm"> {reviews.job}</p>
       </div>
 
-    <div className="text-violet-400 mx-auto mt-5 ">
-      <FaQuoteLeft/>
-    </div>
+      <div className="text-violet-400 mx-auto mt-5 ">
+        <FaQuoteLeft />
+      </div>
 
-<div className="text-center mt-4 text-slate-500 ">
+      <div className="text-center mt-4 text-slate-500 ">
         <p> {reviews.text}</p>
       </div>
 
-    <div className="text-violet-400 mx-auto mt-5 ">
-      <FaQuoteRight/>
-    </div>
-
-
+      <div className="text-violet-400 mx-auto mt-5 ">
+        <FaQuoteRight />
+      </div>
     </div>
   );
 };
--- week1/Testimonial-slider/src/components/Testimonial.jsx
+++ week1/Testimonial-slider/src/components/Testimonial.jsx
@@ -5,52 +5,53 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 const Testimonial = (props) => {
   let reviews = props.reviews;
 
-  const[index,setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
 
-  function LeftShiftHandler(){
-    if(index - 1 < 0){
-      setIndex(reviews.length-1);
-    }
-    else{
-      setIndex(index-1);
+  function LeftShiftHandler() {
+    if (index - 1 < 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
     }
   }
 
-  function RightShiftHandler(){
-    if(index + 1 >= reviews.length){
+  function RightShiftHandler() {
+    if (index + 1 >= reviews.length) {
       setIndex(0);
-    }
-    else{
-      setIndex(index+1);
+    } else {
+      setIndex(index + 1);
     }
   }
 
-  function SurpriseMeHandler(){
-      let randomIndex = Math.floor(Math.random() * reviews.length);
-      setIndex(randomIndex);
+  function SurpriseMeHandler() {
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    setIndex(randomIndex);
   }
 
   return (
-    <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center mt-10 p-10 tranisition-all duration-700 hover:shadow-xl">
+    <div className="w-[85vw] md:w-[700px] bg-white flex flex-col justify-center mt-10 p-10 tranisition-all duration-700 hover:shadow-xl rounded-md">
       <Card reviews={reviews[index]} />
 
-      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold text-center mx-auto">
+      <div className="flex text-3xl mt-10 gap-3 text-violet-400 font-bold text-center mx-auto">
         <button
-        onClick={LeftShiftHandler} 
-        className="cursor-pointer hover:text-violet-500">
+          onClick={LeftShiftHandler}
+          className="cursor-pointer hover:text-violet-500"
+        >
           <FiChevronLeft />
         </button>
         <button
-        onClick={RightShiftHandler} 
-        className="cursor-pointer hover:text-violet-500">
+          onClick={RightShiftHandler}
+          className="cursor-pointer hover:text-violet-500"
+        >
           <FiChevronRight />
         </button>
       </div>
 
-      <div>
-        <button 
-        onClick={SurpriseMeHandler} 
-        className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
+      <div className="mt-5">
+        <button
+          onClick={SurpriseMeHandler}
+          className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg "
+        >
           Surprise me
         </button>
       </div>
