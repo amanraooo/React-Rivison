a5360cbec62c6833c46a0e728637f5e5fefe2ab7
amanraooo <ak9070777@gmail.com>
2025-10-13 11:01:09 +0530

    testimonial slider project

--- week1/Testimonial-slider/src/components/Card.jsx
+++ week1/Testimonial-slider/src/components/Card.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import {FaQuoteLeft,FaQuoteRight, FaGem} from "react-icons/fa";
 
 const Card = (props) => {
-    let reviews = props.reviews
+  let reviews = props.reviews;
   return (
     <div>
-      
+
+      <div>
+        <img src={reviews.image} />
+      </div>
+
+      <div>
+        <p>{reviews.name} </p>
+      </div>
+
+      <div>
+        <p> {reviews.job}</p>
+      </div>
+
+    <div>
+      <FaQuoteLeft/>
     </div>
-  )
-}
-export default Card
+
+<div>
+        <p> {reviews.text}</p>
+      </div>
+
+    <div>
+      <FaQuoteRight/>
+    </div>
+
+    <div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+
+<div>
+  <button>Surprise me </button>
+</div>
+
+    </div>
+  );
+};
+export default Card;
--- week1/Testimonial-slider/src/components/Testimonial.jsx
+++ week1/Testimonial-slider/src/components/Testimonial.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 const Testimonial = (props) => {
-     let reviews = props.reviews;
-     
+  let reviews = props.reviews;
+
   return (
     <div>
-      <Card reviews={reviews[0]}/>
+      <Card reviews={reviews[0]} />
     </div>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
