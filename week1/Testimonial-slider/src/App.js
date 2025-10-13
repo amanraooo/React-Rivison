dddde99f61ac073c79ecb72f7ab524ac7957b17e
amanraooo <ak9070777@gmail.com>
2025-10-13 21:44:20 +0530

    testimonial-slider-project

--- week1/Testimonial-slider/src/App.js
+++ week1/Testimonial-slider/src/App.js
@@ -4,10 +4,10 @@ import reviews from './data';
 
 const App = () => {
   return (
-    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-[#0A0A0A]">
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Our Testimonials </h1>
-        <div className="bg-violet-400 h-[4px] w-1/5 mx-auto mt-1"> </div>
+        <h1 className="text-4xl font-bold font-['Orbitron'] text-[#FFD700]"> The Justice League</h1>
+        <div className="bg-gradient-to-r from-[#0077FF] to-[#C62828] h-[4px] w-1/5 mx-auto mt-2 rounded-full shadow-lg shadow-[#0077FF]/50"></div>
         <Testimonial reviews={reviews} />
       </div>
     </div>
