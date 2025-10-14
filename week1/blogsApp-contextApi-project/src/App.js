c3d265120437a4ef756fd88728969962399c0644
amanraooo <ak9070777@gmail.com>
2025-10-14 21:28:12 +0530

    blogs app

---
+++ week1/blogsApp-contextApi-project/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Blogs from './components/Blogs';
+import Pagination from './components/Pagination';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Blogs />
+      <Pagination />
+    </div>
+  );
+}
+export default App
