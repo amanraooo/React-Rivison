b95d8e897eb5e1b1c0e9769e42f83daa1c032f7c
amanraooo <ak9070777@gmail.com>
2025-10-15 23:27:44 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/context/AppContext.js
+++ week1/blogsApp-contextApi-project/src/context/AppContext.js
@@ -3,7 +3,7 @@ import { baseUrl } from './../baseUrl';
 
 export const AppContext = createContext();
 
-function AppContextProvider({ children }) {
+export  default function AppContextProvider({ children }) {
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
