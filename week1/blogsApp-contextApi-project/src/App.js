bbe29708d87372c9fe303eb36d3c5c94a70ed9a1
amanraooo <ak9070777@gmail.com>
2025-10-16 19:33:37 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/App.js
+++ week1/blogsApp-contextApi-project/src/App.js
@@ -1,9 +1,17 @@
 import Header from './components/Header';
 import Blogs from './components/Blogs';
 import Pagination from './components/Pagination';
-
+import { useContext, useEffect } from 'react';
+import { AppContext } from './context/AppContext';
+import './App.css'
 
 function App() {
+  const {fetchBlogPosts} =useContext(AppContext);
+
+  useEffect(()=>{
+    fetchBlogPosts();
+  },[]);
+
   return (
     <div>
       <Header />
--- week1/blogsApp-contextApi-project/src/components/Blogs.jsx
+++ week1/blogsApp-contextApi-project/src/components/Blogs.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
 import { AppContext } from './../context/AppContext';
-import Spinner from './../../../top-course-starter/src/components/Spinner';
+import Spinner from './Spinner';
+
 
 const Blogs = () => {
-  const {loading} = useContext(AppContext);
+  const {loading,posts} = useContext(AppContext);
   return (
     <div>
       {
@@ -15,7 +16,22 @@ const Blogs = () => {
           (<div>
             <p>No Post Found</p>
           </div>):
-          (posts.map((post)=> (<Card/>)))
+          (posts.map((post)=> (
+            <div key={post.id}>
+              <p className='font-bold'>{post.title}</p>
+              <p>
+                By <span>{post.author}</span>
+                on <span>{post.category}</span>
+              </p>
+              <p>Posted on {post.date}</p>
+              <p>{post.content}</p>
+              <div>
+                {post.tags.map((tag, index)=>{
+                  return <span key={index}>{`#${tag}`}</span>
+                })}
+              </div>
+            </div>
+          )))
         )
       }
     </div>
