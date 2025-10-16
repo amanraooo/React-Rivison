3ec237e006f420635c2e89ce8f2d7b1c6b2fbd87
amanraooo <ak9070777@gmail.com>
2025-10-16 10:48:57 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/components/Blogs.jsx
+++ week1/blogsApp-contextApi-project/src/components/Blogs.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './../context/AppContext';
+import Spinner from './../../../top-course-starter/src/components/Spinner';
 
 const Blogs = () => {
+  const {loading} = useContext(AppContext);
   return (
     <div>
-      
+      {
+        loading ? 
+        (<Spinner/>) :
+       
+        (
+          posts.length === 0 ?
+          (<div>
+            <p>No Post Found</p>
+          </div>):
+          (posts.map((post)=> (<Card/>)))
+        )
+      }
     </div>
   )
 }
