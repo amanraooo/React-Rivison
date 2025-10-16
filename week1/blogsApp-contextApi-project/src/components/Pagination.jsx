be55b865ea1fdbd58212fdedb1e106021373f0e6
amanraooo <ak9070777@gmail.com>
2025-10-16 19:45:21 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/components/Pagination.jsx
+++ week1/blogsApp-contextApi-project/src/components/Pagination.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Pagination = () => {
+  const { page, handlePageChange, totalPages } = useContext(AppContext);
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div>
+        {/* prev button */}
+        {
+        page > 1 && 
+        (
+          <button onClick={() => handlePageChange(page - 1)}>Previous</button>
+        )
+        }
 
-export default Pagination
+        {/* next button */}
+        {
+        page < totalPages && 
+        (
+          <button onClick={() => handlePageChange(page + 1)}>Next</button>
+        )
+        }
+
+        {/* show curr page */}
+        <p>
+          Page {page} of {totalPages}
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
