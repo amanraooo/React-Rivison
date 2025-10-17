7eb02908107f5f95df3ba97cafb50b5111757980
amanraooo <ak9070777@gmail.com>
2025-10-17 14:58:07 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/App.js
+++ week1/blogsApp-contextApi-project/src/App.js
@@ -4,6 +4,7 @@ import Pagination from './components/Pagination';
 import { useContext, useEffect } from 'react';
 import { AppContext } from './context/AppContext';
 import './App.css'
+import { Routes , Route} from 'react-router-dom';
 
 function App() {
   const {fetchBlogPosts} =useContext(AppContext);
@@ -14,9 +15,12 @@ function App() {
 
   return (
     <div className='w-full h-full flex flex-col gap-y-1 justify-center items-center'>
-      <Header />
-      <Blogs />
-      <Pagination />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/blog/:blogId' element={<BlogPage/>}/>
+        <Route path='/tags/:tag' element={<TagPage/>}/>
+        <Route path='/categories/:category' element={<CategoryPage/>}/>      
+      </Routes>
     </div>
   );
 }
--- week1/blogsApp-contextApi-project/src/context/AppContext.js
+++ week1/blogsApp-contextApi-project/src/context/AppContext.js
@@ -9,9 +9,18 @@ export  default function AppContextProvider({ children }) {
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(null);
 
-    async function fetchBlogPosts(page=1){
+    async function fetchBlogPosts(page=1, tag=null, category){
+
         setLoading(true);
+        
         let url = `${baseUrl}?page=${page}`;
+
+        if(tag){
+            url += `&tag=${tag}`;            
+        }
+        if(category){
+            url += `&tag=${category}`;
+        }
 
         try{
             const result = await fetch(url);
