bd2d3758eaef30b0a083cd40051de51c3fe057bd
amanraooo <ak9070777@gmail.com>
2025-10-17 16:59:06 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/App.js
+++ week1/blogsApp-contextApi-project/src/App.js
@@ -1,10 +1,11 @@
-import Header from './components/Header';
-import Blogs from './components/Blogs';
-import Pagination from './components/Pagination';
 import { useContext, useEffect } from 'react';
 import { AppContext } from './context/AppContext';
 import './App.css'
 import { Routes, Route, useSearchParams, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import BlogPage from './pages/BlogPage';
+import TagPage from './pages/TagPage';
+import CategoryPage from './pages/CategoryPage';
 
 function App() {
   const { fetchBlogPosts } = useContext(AppContext);
@@ -31,14 +32,14 @@ function App() {
   }, [location.pathname, location.search]);
 
   return (
-    <div className='w-full h-full flex flex-col gap-y-1 justify-center items-center'>
+    
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/blog/:blogId' element={<BlogPage />} />
         <Route path='/tags/:tag' element={<TagPage />} />
         <Route path='/categories/:category' element={<CategoryPage />} />
       </Routes>
-    </div>
+   
   );
 }
 export default App
