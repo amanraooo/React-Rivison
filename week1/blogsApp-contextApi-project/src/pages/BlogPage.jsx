f27a4c59b884e8394f18fe03382778b10be45a07
amanraooo <ak9070777@gmail.com>
2025-10-22 17:45:53 +0530

    context api project

--- week1/blogsApp-contextApi-project/src/pages/BlogPage.jsx
+++ week1/blogsApp-contextApi-project/src/pages/BlogPage.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useLocation, useNavigation } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { baseUrl } from '../baseUrl';
 
 const BlogPage = () => {
+
+  const [blog, setBlog] = useState(null);
+  const [relatedBlogs,setRelatedBlogs] =  useState([]);
+  const location = useLocation();
+  const  navigation = useNavigation();
+  const {setLoading, loading} = useContext(AppContext);
+
+  const  blogId = location.pathname.split("/").at(-1);
+
+  async function fetchRelatedBlogs(){
+    setLoading(true);
+    let url =`${baseUrl}?blogId=${blogId}`;
+
+    try{
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setBlog(data.blog);
+      setRelatedBlogs(data.relatedBlogs);
+
+    }catch(error){
+      console.log("error");
+      setBlog(null);
+      setRelatedBlogs([]);
+
+    }
+  }
+
+  useEffect(()=>{
+    if(blogId){
+      fetchRelatedBlogs();
+    }
+  },[location.pathname])
+  
   return (
     <div>
       
