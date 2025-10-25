ea653a72e180d993b3a9a4caf333b1b9c10c706a
amanraooo <ak9070777@gmail.com>
2025-10-25 16:25:09 +0530

    redux project

--- week1/redux-project/src/pages/Cart.jsx
+++ week1/redux-project/src/pages/Cart.jsx
@@ -1,5 +1,36 @@
+import { useSelector } from 'react-redux';
+import CartItem from './../components/CartItem';
+import { Link } from 'react-router-dom';
+
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const {cart} = useSelector((state)=>state);
+  return (
+  <div>
+    {
+      cart.length > 0 ?
+      (
+        <div>
+          <div>
+            {
+              cart.map((item,index)=>{
+                return <CartItem key={item.id} item={item} itemIndex={index}/>
+              })
+            }
+          </div>
+        </div>
+      ):
+      (
+        <div>
+          <h1>Cart empty</h1>
+          <Link to="/">
+          <button>Shop Now</button>
+          </Link>
+        </div>
+      )
+    }
+    </div>
+    );
 };
 
 export default Cart;
