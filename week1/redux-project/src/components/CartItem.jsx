a90fac0c4983fbd774afe52eeb56b6404bef4181
amanraooo <ak9070777@gmail.com>
2025-10-26 22:02:18 +0530

    redux project

--- week1/redux-project/src/components/CartItem.jsx
+++ week1/redux-project/src/components/CartItem.jsx
@@ -1,41 +1,43 @@
-import { FcDeleteDatabase } from "react-icons/fc"
-import { useDispatch } from 'react-redux';
-import {remove} from "../redux/Slices/CartSlice";
+import { FcDeleteDatabase } from "react-icons/fc";
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
 import { toast } from "react-hot-toast";
 
-
-const CartItem = ({ item, itemIndex }) => {
-
+const CartItem = ({ item }) => {
   const dispatch = useDispatch();
 
   const removeFromCart = () => {
-    dispatch(remove(item.id))
-    toast.error("removed from cart");
-  }
+    dispatch(remove(item.id));
+    toast.error("Removed from cart");
+  };
 
   return (
-    <div>
-      <div>
+    <div className="flex flex-col sm:flex-row justify-between items-center bg-white p-5 rounded-2xl shadow-md border border-gray-200">
+      <div className="w-40 h-40 flex justify-center items-center">
+        <img
+          src={item?.image || ""}
+          alt={item?.title || "Product"}
+          className="object-contain max-h-full"
+        />
+      </div>
 
-        <div>
-          <img src={item.image} alt="itemimg" />
-        </div>
+      <div className="flex flex-col sm:w-2/3 mt-4 sm:mt-0 sm:ml-6">
+        <h1 className="text-lg font-semibold text-gray-800 mb-1">
+          {item?.title}
+        </h1>
+        <p className="text-sm text-gray-600 mb-3">{item?.description}</p>
 
-        <div>
-          <h1>{item.title}</h1>
-          <h1>{item.description}</h1>
-          <div>
-            <p>{item.price}</p>
-            <div
-              onClick={removeFromCart}
-            >
-              <FcDeleteDatabase />
-            </div>
-          </div>
+        <div className="flex justify-between items-center">
+          <p className="text-green-700 font-bold text-lg">${item?.price}</p>
+          <button
+            onClick={removeFromCart}
+            className="text-red-500 hover:text-red-700 transition"
+          >
+            <FcDeleteDatabase size={26} />
+          </button>
         </div>
       </div>
     </div>
-
   );
 };
 
