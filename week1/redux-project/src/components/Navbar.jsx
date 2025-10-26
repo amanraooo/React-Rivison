8c514a4b502204ecc4c06aa2845a93e6e5af299a
amanraooo <ak9070777@gmail.com>
2025-10-26 17:14:57 +0530

    redux project

--- week1/redux-project/src/components/Navbar.jsx
+++ week1/redux-project/src/components/Navbar.jsx
@@ -1,29 +1,38 @@
 import { NavLink } from 'react-router-dom';
 import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+  const { cart } = useSelector((state) => state)
+
   return (
     <div>
-      <div className="flex flex-row justify-between">
+      <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
         <NavLink to="/">
-          <div>
-            <img src="" alt="img" />
+          <div className="ml-5">
+            <img src="../logo.png" alt="img" className="h-14" />
           </div>
         </NavLink>
 
-        <div>
+        <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
           <NavLink to="/">
             <p>Home</p>
           </NavLink>
 
           <NavLink to="/cart">
-            <div>
-              <FaShoppingCart />
+            <div className="relative">
+              <FaShoppingCart className='text-2xl' />
+              {
+                cart.length > 0 && <span
+                  className='absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white'
+                >{cart.length}</span>
+              }
+
             </div>
           </NavLink>
 
         </div>
-      </div>
+      </nav>
 
     </div>
   );
