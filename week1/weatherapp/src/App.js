a47e0f6462f6abbf0b183b9cc32ff37e1142b327
amanraooo <ak9070777@gmail.com>
2025-11-01 23:46:06 +0530

    weather app

--- week1/weatherapp/src/App.js
+++ week1/weatherapp/src/App.js
@@ -1,30 +1,29 @@
 import { useState } from "react";
 import Loader from "./components/Loader";
 
-
 function App() {
 
-  const[city,setCity] = useState("");
-  const[data,setData] = useState(null);
-  const[loading,setloading]= useState(false);
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setloading] = useState(false);
 
   const API_KEY = "980f15aef7f437b5aea453f4b17a8b51"
 
-  const handleSearh= async ()=>{
+  const handleSearh = async () => {
 
-    if(!city.trim) return;
+    if (!city.trim) return;
 
     setloading(true);
     setData(null);
 
-    try{
-     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
 
-     const data = await res.json();
+      const data = await res.json();
 
       setData(data);
       setloading(false);
-    }catch(e){
+    } catch (e) {
 
     }
   }
@@ -32,27 +31,26 @@ function App() {
   return (
     <div>
       <input
-      type="text"
-      placeholder="enter city"
-      value={city}
-      onChange={(e)=>{setCity(e.target.value)}}
+        type="text"
+        placeholder="enter city"
+        value={city}
+        onChange={(e) => { setCity(e.target.value) }}
       />
 
       <button onClick={handleSearh}>Search</button>
 
       {
-        loading && <Loader/>
+        loading && <Loader />
       }
 
       {
         data && data.main && (
           <div>
-          <h2>{data.name}, {data.sys.country}</h2>
-         
+            <h2>{data.name}, {data.sys.country}</h2>
             <h1>{data.main.temp}°C</h1>
             <p>Feels like: {data.main.feels_like}°C</p>
-          <p>Humidity: {data.main.humidity}%</p>
-          <p>Wind: {data.wind.speed} m/s</p>
+            <p>Humidity: {data.main.humidity}%</p>
+            <p>Wind: {data.wind.speed} m/s</p>
           </div>
         )
       }
