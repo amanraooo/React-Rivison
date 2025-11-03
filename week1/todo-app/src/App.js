3de1ecee3c08124259ec66d1bf46f1e360da7c0b
amanraooo <ak9070777@gmail.com>
2025-11-03 21:41:13 +0530

    todo app

--- week1/todo-app/src/App.js
+++ week1/todo-app/src/App.js
@@ -6,16 +6,39 @@ function App() {
   const [task, setTask] = useState("")
   const [todos, setTodos] = useState([]);
 
+  // add todo
   const addTodo = () => {
     if (task.trim() === "") return;
 
-    setTodos([...todos, task]);
+    const newTodo = {
+      id: Date.now(),
+      text: task,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo]);
     setTask("");
+  }
+
+  // delete todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(t => t.id !== id))
+  }
+
+  //Toggle Todo
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(t =>
+        t.id === id ?
+          { ...t, completed: !t.completed } : t
+      )
+    )
   }
 
   return (
     <div>
       <input
+        type="text"
         placeholder="Enter task"
         value={task}
         onChange={(e) => setTask(e.target.value)}
@@ -25,8 +48,18 @@ function App() {
 
       <div>
         {
-          todos.map((todo, i) => (
-            <p key={i}>{todo}</p>
+          todos.map((todo) => (
+            <div key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => toggleTodo(todo.id)}
+              />
+
+              <span >{todo.text}</span>
+
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
           ))
         }
       </div>
